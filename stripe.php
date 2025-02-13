237409abc6f1f63f5b5b1e7b2688a071e6e0825d
Vincent Barbay <vincent.barbay@gmail.com>
2025-02-13 12:49:57 +0100

    v3.9.6

--- stripe.php
+++ stripe.php
@@ -114,7 +114,7 @@ class Stripe extends PaymentModule
     {
         $this->name = 'stripe';
         $this->tab = 'payments_gateways';
-        $this->version = '3.9.4';
+        $this->version = '3.9.6';
         $this->author = 'Cybor SA';
         $this->need_instance = 0;
 
